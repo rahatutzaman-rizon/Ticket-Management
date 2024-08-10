e2d90fbd318312fb035febd1c9889aff204ed82c
Rahatutzaman Rizon <115326466+rahatutzaman-rizon@users.noreply.github.com>
2024-08-10 23:23:32 +0600

    add svg

--- src/Components/Dashboard/Overview.jsx
+++ src/Components/Dashboard/Overview.jsx
@@ -1,6 +1,5 @@
-
+import React from 'react';
 import { FaArrowUp, FaArrowDown, FaChartLine, FaShoppingBag, FaCreditCard } from 'react-icons/fa';
-import LineChart from './LineChart';
 
 const OverviewCard = ({ icon, title, value, change, isPositive, bgColor, chartColor }) => {
 
@@ -17,35 +16,33 @@ const OverviewCard = ({ icon, title, value, change, isPositive, bgColor, chartCo
     }
   };
 
-
   return (
     <div className={`p-4 shadow-sm rounded-lg flex ${bgColor}`}>
       {/* Left side with text content */}
       <div className="flex flex-col justify-between w-2/3">
-      <div className="flex items-center">
-         
-      <div className="p-2 rounded-full bg-black text-white mr-2">
+        <div className="flex items-center">
+          <div className="p-2 rounded-full bg-black text-white mr-2">
             {getIcon()}
           </div>
-
-          
         </div>
         <div className="mt-1">
           <h4 className="font-medium text-black text-xs">{title}</h4>
         </div>
         <div className="text-3xl font-bold text-gray-800">{value}</div>
         <div
-        className={`mt-1 p-1 bg-white flex items-center rounded-full ${isPositive ? 'text-green-600' : 'text-red-600'}`}
-        style={{ fontSize: '12px' }} // Replace '12px' with your desired font size
-      >
-        {isPositive ? <FaArrowUp className="mr-1" /> : <FaArrowDown className="mr-1" />}
-        <span style={{ fontSize: '8px' }}>{change} <span className='text-black'> this week</span></span> {/* Replace '12px' with your desired font size */}
-      </div>
+          className={`mt-1 p-1 bg-white flex items-center rounded-full ${isPositive ? 'text-green-600' : 'text-red-600'}`}
+          style={{ fontSize: '12px' }}
+        >
+          {isPositive ? <FaArrowUp className="mr-1" /> : <FaArrowDown className="mr-1" />}
+          <span style={{ fontSize: '8px' }}>{change} <span className='text-black'> this week</span></span>
+        </div>
       </div>
       
-      {/* Right side with line chart */}
-      <div className="flex justify-center items-center w-1/3  ">
-        <LineChart chartColor={chartColor} />
+      {/* Right side with SVG line chart */}
+      <div className="flex justify-center items-center w-1/3">
+        <svg width="100%" height="40" viewBox="0 0 100 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 30 Q25 10, 50 20 T100 30" stroke={chartColor} strokeWidth="2" fill="transparent" />
+        </svg>
       </div>
     </div>
   );
